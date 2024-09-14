4666523fda0433b0d0b9ba9aef2589efc6e0d295
Yoe <dembouz719@gmail.com>
2024-09-14 19:02:49 +0200

    create for-loop, hand_shape

--- new_rock_paper_scissor_app/source/game.cpp
+++ new_rock_paper_scissor_app/source/game.cpp
@@ -3,6 +3,13 @@
 
 void game::startGame(std::vector<Player*> players, std::vector<hand_shape> hand_shapeVector) {
 
+	for (Player* player : players) {
+		std::cout << "Player: " << player->getName() << std::endl;
+	}
+
+	for (const hand_shape& : hand_shapeVector) {
+		std::cout << "Hand shape: " << shape.getType() << std::endl;
+	}
 
 };
 
